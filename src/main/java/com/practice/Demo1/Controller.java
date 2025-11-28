20bf2e36dbf7bfd27803cd8033881acf085257e7
bikashpradhan4692@gmail.com <bikashpradhan4692@gmail.com>
2025-11-28 18:20:58 +0530

    demo-app-v1

---
+++ src/main/java/com/practice/Demo1/Controller.java
@@ -0,0 +1,12 @@
+package com.practice.Demo1;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/message")
+    public String getMessage(){
+        return "welcome to docker";
+    }
+}
